bdbd4b00d5eba1a49d3e635a0c32d1a8c6a7afa5
student <student@EDAs-MacBook-Pro.local>
2025-02-20 15:02:01 -0600

    endpoint for additem created

--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -20,5 +20,26 @@ router.get("/", rejectUnauthenticated, (req, res) => {
       });
 }); 
 
+// Post to add a new item
+router.post('/add-item', rejectUnauthenticated, async (req, res) => {
+  const { name, description, image_url, created_by} = req.body;
+
+  const sqlText = `
+    INSERT INTO "item" ("name", "description", "image_url", "created_by")
+    VALUES ($1, $2, $3, $4) RETURNING *;
+  `;
+  const values = [name, description, image_url, created_by]; 
+
+  try {
+    const result = await pool.query(sqlText, values);
+    res.status(201).send(result.rows[0]);  // Send back the newly created item
+  } catch (err) {
+    console.log('Error creating new item:', err);
+    res.status(500).send('Failed to create item');
+  }
+});
+
+
+
   module.exports = router;
   
