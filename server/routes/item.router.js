8fc147f82e6547103d679d4ae90672328660b056
Alexander Ruziska <ziskapants@gmail.com>
2025-02-20 14:54:30 -0600

    just kidding this works

--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -8,15 +8,13 @@ const router = express.Router();
 
 
 router.get("/", rejectUnauthenticated, (req, res) => {
-    console.log('req.user', req.item);
-    const sqlText = `SELECT * FROM "item";`;
-
-    pool.query(sqlText, [req.item])
-  .then((dbRes) => {
-    res.send(dbRes.rows);
+    const queryText = `SELECT * FROM "item";`;
+    pool.query(queryText)
+      .then((result) => {
+        res.send(result.rows);
       })
-      .catch((dbErr) => {
-        console.log('GET /api/items error:', dbErr);
+      .catch((err) => {
+        console.log("Error getting items", err);
         res.sendStatus(500);
       });
   });
