0f5dad79a85ba0879a83a17ddf7801db53ba0316
Alecia Hagen <alecia.h98@gmail.com>
2025-02-20 14:51:52 -0600

    almost fully figured out my code

--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 
-const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 
 const router = express.Router();
 
@@ -20,6 +20,23 @@ router.get("/", rejectUnauthenticated, (req, res) => {
         res.sendStatus(500);
       });
   });
+
+
+router.delete("/delete/:id", rejectUnauthenticated, (req, res) => {
+  const query = `
+  DELETE 
+FROM "item"
+WHERE "item"."id" = $1;
+  `;
+  pool.query(query, [req.params.id])
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log(`Error delete backend`, err);
+      res.sendStatus(500);
+    })
+})
 
   module.exports = router;
   
